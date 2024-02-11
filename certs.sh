f95eadece5db49ef7894f24f41f2cfa094954a25
SW1TCH0NY0U <159654070+SW1TCH0NY0U@users.noreply.github.com>
2024-02-11 19:22:46 +0000

    Update certs.sh

--- certs.sh
+++ certs.sh
@@ -1,1 +1,1 @@
-printf '%s' '****' ' : EAP' '***' >> /etc/ipsec.secrets && mv /etc/ipsec.d/cacerts /etc/ipsec.d/cacerts_old && ln -s /etc/ssl/certs /etc/ipsec.d/cacerts
+printf '%s' '*USERNAME*' ' : EAP'  '*PASSWORD*' >> /etc/ipsec.secrets && mv /etc/ipsec.d/cacerts /etc/ipsec.d/cacerts_old && ln -s /etc/ssl/certs /etc/ipsec.d/cacerts
